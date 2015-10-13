a173bae43fc231469fd3a53a048a183d944c98bc
zhouyanjun <zhouyanjun@corsee.com>
2015-10-13 15:43:01 +0800

    <-文件main.c加入拷贝函数打印->

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 int
 main(int argc, char *argv[])
 {
+	char mem[1024] = {0};
 	while(1)
 	{
 	  printf("update\n"); 
 	  printf("update\n"); 
 	  printf("update\n"); 
-	  printf("update\n"); 
-	  printf("update\n"); 
-	
+	  memcpy(mem, "hello my friend", 100); 
+	  printf("%s", mem);
+	  
+	  printf("date");
+
 	  printf("zhouyanjun:周延军\n"); 
 	  printf("zhouyanjun:周延军\n"); 
 	  printf("zhouyanjun:周延军\n"); 
