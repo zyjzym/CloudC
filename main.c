bf0d9380f32dfe68c4e1743ff1cebd4ce70e9e6b
zhouyanjun <zhouyanjun@corsee.com>
2015-10-13 15:25:39 +0800

    <-创建文件main.c->

---
+++ main.c
@@ -0,0 +1,16 @@
+
+int
+main(int argc, char *argv[])
+{
+	while(1)
+	{
+	  printf("update\n"); 
+	  printf("update\n"); 
+	  printf("update\n"); 
+	  printf("update\n"); 
+	  printf("update\n"); 
+	
+	  printf("zhouyanjun:周延军\n"); 
+	}
+	return 0;
+}
